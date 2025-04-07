b6a5287b4bc97b9eb8db1c670083d9aac4677f81
RITika2522 <ritikachandak2502@gmail.com>
2025-04-07 17:08:17 +0530

    dictionary data structure

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#ordered set of pairs
+#order is based on keys
+#mutable data structure values can be changed
+#when the key is again used with different value than it would be considering the latest value as it is mutable
+
+# dictvar = {"Name" : "Amit", "Program" : "MCA", "Percentage" : 89.90, "City" : "Pune"}
+
+dictvar = {1:"Amit",2:"MCA",3:89.9,"Address":["Mit","Kothrud","Pune"]}
+
+print(dictvar["Address"])
--- setds.py
+++ setds.py
@@ -7,10 +7,10 @@
 # sobj=(2,6,4,1,5,3)
 # sobj=(4,5,1,2,3,90,30,20,8,9)
 # sobj= ("amit",3,"Ajay",10,"rajesh",6,"omkar",5)
-sobj = {"amit", 101, "ajay", 101, 89.9, "pune"}
+""" sobj = {"amit", 101, "ajay", 101, 89.9, "pune"}
 print(type(sobj))
 for val in sobj:
-    print(val, end=" ")
+    print(val, end=" ") """
 
 
 #add
@@ -20,9 +20,26 @@ for val in sobj:
 #remove
     #will remove the value from the set
     #if the value is not present in the set then it will throw an error
-sobj ={"amit", 101, "ajay", 101, 89.9, "pune"}  
+
+
+""" sobj ={"amit", 101, "ajay", 101, 89.9, "pune"}  
 print("Before remove set:",sobj)
 sobj.remove("amit")
 print("After remove set:",sobj)
 sobj.discard(101)
-print("After discard set:",sobj)
+print("After discard set:",sobj) """
+
+
+set1 ={1,2,3,4}
+alist=['A', 'B','C',1,2,3]
+
+#set1.update(alist)
+
+# strvar = "my name is XYZ"
+# print(strvar.split())       #default value in split is space
+# set1.update(strvar)
+
+tupvar= ("ABCD",)
+temp = tupvar[0]
+set1.update(temp)
+print("Set: ",set1, "Length: ", len(set1))
